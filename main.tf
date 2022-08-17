52a1986fdb7f09190fc576642d184440dcd9ba30
Grant Orchard <go@hashicorp.com>
2022-08-17 10:29:37 +1000

    populate ip_addr expressions

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ module "initial_test" {
 	destination_ports = [
 		443
 	]
-	destinations = []
-	sources = []
+	destinations = ["10.0.0.0/24"]
+	sources = ["192.168.20.0/24"]
 	action = "allow"
 }
 
 
-# DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=ba1f4cb12fb31f2b6f321d2cfd2d119d DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
+
