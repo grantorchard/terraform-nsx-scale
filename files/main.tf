798e6469e6946ecb5d86cc5336d9ce91c5b662ad
Grant Orchard <go@hashicorp.com>
2022-08-18 12:29:30 +1000

    add tf json generator, json files

---
+++ files/main.tf
@@ -0,0 +1,16 @@
+
+
+resource "local_file" "foo" {
+		count = 500
+    content  = jsonencode({
+			display_name = "app-id-${sum([123456, count.index])}",
+			protocol = "tcp",
+			destination_ports = [
+				443
+			],
+			destinations = ["10.0.0.0/24"],
+			sources = ["192.168.20.0/24"],
+			action = "allow"
+		})
+    filename = "${path.module}/app-id-${sum([123456, count.index])}.json"
+}
