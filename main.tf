98f3a62d9ded7e5e530b8da54b28b9e2c2d34f26
Grant Orchard <go@hashicorp.com>
2022-08-26 13:43:57 +1000

    initial commit for aws

--- main.tf
+++ main.tf
@@ -5,17 +5,31 @@ locals {
   raw_inputs = [for v in local.group_files : jsondecode(file(v))]
 }
 
-module "initial_test" {
-	for_each = { for v in local.raw_inputs: v.display_name => v }
-	source = "./modules"
 
-	display_name = each.value.display_name
-	protocol = each.value.protocol
-	destination_ports = each.value.destination_ports
-	destinations = each.value.destinations
-	sources = each.value.sources
-	action = upper(each.value.action)
+
+module "security-group" {
+	for_each = { for v in local.raw_inputs: v.display_name => v }
+
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+	rules = "${each.value.display_name}-${join(",", each.value.ports)}"
+	"zookeeper-2888-tcp": [ 2888, 2888, "tcp", "Zookeeper" ]
 }
+
+
+
+# module "initial_test" {
+# 	for_each = { for v in local.raw_inputs: v.display_name => v }
+# 	source = "./modules"
+
+# 	display_name = each.value.display_name
+# 	protocol = each.value.protocol
+# 	destination_ports = each.value.destination_ports
+# 	destinations = each.value.destinations
+# 	sources = each.value.sources
+# 	action = upper(each.value.action)
+# }
 
 
 
